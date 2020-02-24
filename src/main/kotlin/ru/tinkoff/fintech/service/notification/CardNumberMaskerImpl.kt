2aa7f950902c90962d10337952802af0613a3964
Nartov Maksim <haptob@gmail.com>
2020-02-24 20:19:10 +0300

    homework-1 final

--- src/main/kotlin/ru/tinkoff/fintech/service/notification/CardNumberMaskerImpl.kt
+++ src/main/kotlin/ru/tinkoff/fintech/service/notification/CardNumberMaskerImpl.kt
@@ -1,8 +1,17 @@
 package ru.tinkoff.fintech.service.notification
 
-class CardNumberMaskerImpl: CardNumberMasker {
+class CardNumberMaskerImpl : CardNumberMasker
+{
 
-    override fun mask(cardNumber: String, maskChar: Char, start: Int, end: Int): String {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    override fun mask(cardNumber: String, maskChar: Char, start: Int, end: Int): String
+    {
+        //TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+
+        when
+        {
+            cardNumber == ""      -> return ""
+            end>cardNumber.length -> return "################"
+            else                  -> return cardNumber.replaceRange(start,end, maskChar.toString().repeat(end-start))
+        }
     }
 }
