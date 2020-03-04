07d2de3cfb3c3df52bb544f2430dd5e9df935d32
Nartov Maksim <haptob@gmail.com>
2020-03-04 20:17:12 +0300

    All, Black, TripleSIx

--- src/main/kotlin/ru/tinkoff/fintech/service/cashback/CashbackCalculatorImpl.kt
+++ src/main/kotlin/ru/tinkoff/fintech/service/cashback/CashbackCalculatorImpl.kt
@@ -1,6 +1,10 @@
 package ru.tinkoff.fintech.service.cashback
 
 import ru.tinkoff.fintech.model.TransactionInfo
+import ru.tinkoff.fintech.service.cashback.modes.All
+import ru.tinkoff.fintech.service.cashback.modes.Black
+import ru.tinkoff.fintech.service.cashback.modes.TripleSix
+import kotlin.math.round
 
 internal const val LOYALTY_PROGRAM_BLACK = "BLACK"
 internal const val LOYALTY_PROGRAM_ALL = "ALL"
@@ -12,7 +16,17 @@ internal const val MCC_BEER = 5921
 class CashbackCalculatorImpl : CashbackCalculator {
 
     override fun calculateCashback(transactionInfo: TransactionInfo): Double {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        // TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        val modes = listOf(Black(), TripleSix(), All())
+        var result: Double = .0
+        //var result2 = modes.reduce {result, mode ->  result + mode.calculate(transactionInfo)}
+
+        for (f in modes) {
+            result += f.calculate(transactionInfo)
+        }
+
+        return minOf(3000.0 - transactionInfo.cashbackTotalValue, result)
     }
 
 }
+
